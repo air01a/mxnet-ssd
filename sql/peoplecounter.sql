9c7307aac3429d70bcf5d3906524e2549379114c
Erwan <Erwan@ydhtk.com>
2018-05-11 08:04:04 +0000

    sql statement

---
+++ sql/peoplecounter.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `peoplecounter` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `timer` datetime DEFAULT NULL,
+  `counter` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
